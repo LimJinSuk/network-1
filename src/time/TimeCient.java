1a268ae73bf1f63a41a5c9863d012ba2b8a11c13
bit-user <bit-user@bit>
2017-04-24 15:30:35 +0900

    time c/s 완성

--- src/time/TimeCient.java
+++ src/time/TimeCient.java
@@ -1,8 +1,44 @@
 package time;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
 public class TimeCient {
-
+	private static final String SERVER_IP = "192.168.1.39";
+	private static final int SERVER_PORT = 7070;
+	private static final int BUFFER_SIZE = 1024;
+	
 	public static void main(String[] args) {
-	}
+		DatagramSocket socket = null;
 
+		try {
+			socket = new DatagramSocket();
+
+			String message = "";
+			byte[] sendData = message.getBytes("utf-8");
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
+					new InetSocketAddress(SERVER_IP, SERVER_PORT));
+			socket.send( sendPacket );
+			
+			DatagramPacket receivePacket = new DatagramPacket( new byte[ BUFFER_SIZE ], BUFFER_SIZE, new InetSocketAddress( SERVER_IP, SERVER_PORT ) );
+			socket.receive( receivePacket  );
+			String datetime = new String( receivePacket.getData(), 0, receivePacket.getLength(), "utf-8" );
+			System.out.println( datetime );
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if( socket != null && socket.isClosed() == false ) {
+				socket.close();
+			}
+		}
+	}
 }
--- src/time/TimeServer.java
+++ src/time/TimeServer.java
@@ -1,6 +1,44 @@
 package time;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeServer {
+	private static final int PORT = 7070;
+	private static final int BUFFER_SIZE = 1024;
+
 	public static void main(String[] args) {
+		DatagramSocket socket = null;
+
+		try {
+			// 소켓 생성
+			socket = new DatagramSocket(PORT);
+
+			while (true) {
+				// 수신 패킷 생성
+				DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+
+				// 수신
+				socket.receive(receivePacket);
+
+				// 시간 전송
+				String datetime = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss").format(new Date());
+				byte[] sendData = datetime.getBytes( "utf-8" );
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getSocketAddress());
+				socket.send(sendPacket);
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
 	}
 }
